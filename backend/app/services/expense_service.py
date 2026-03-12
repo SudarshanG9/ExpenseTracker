525c7736f5d48ed5b01246b8a842f9bc89d7d868
Sudarshan G <95075579+SudarshanG9@users.noreply.github.com>
2026-03-12 09:40:42 +0530

    expense service commited

--- backend/app/services/expense_service.py
+++ backend/app/services/expense_service.py
@@ -1,36 +1,64 @@
-from app.models.expenses import ExpenseCreate, ExpenseUpdate, ExpenseResponse
-
-temp_expense = []
-expense_id = 1
-
-
-def create_expense(expense: ExpenseCreate) -> ExpenseResponse:
-    global expense_id
-    newExpense = expense.dict()
-
-    newExpense['id'] = expense_id
-    expense_id += 1
-    temp_expense.append(newExpense)
-    return newExpense
+from sqlalchemy.orm import Session 
+from app.models.expense import Expense
+from app.schemas.expense import ExpenseCreate, ExpenseUpdate
 
 
 
+def create_expense(db: Session, expense_data: ExpenseCreate):
 
-def get_expenses() -> list[ExpenseResponse]:
-    return temp_expense
+    expense = Expense(
+        title = expense_data.title,
+        amount = expense_data.amount,
+        category = expense_data.category,
+        date = expense_data.date
+    )
+    
+    db.add(expense)
+    db.commit()
+    db.refresh(expense)
+    return expense
 
 
-def update_expense(expense_id: int, expense: ExpenseUpdate) -> ExpenseResponse:
-    for expense in temp_expense:
-        if expense['id'] == expense_id:
-            expense.update(expense.dict())
-            return expense
-    return None
+
+def get_expenses(db: Session):
+    return db.query(Expense).all()
 
 
-def delete_expense(expense_id: int) -> ExpenseResponse:
-    for expense in temp_expense:
-        if expense['id'] == expense_id:
-            temp_expense.remove(expense)
-            return expense
-    return None
+def get_expense(db: Session,expense_id: int):
+    return db.query(Expense).filter(Expense.id == expense_id).first()
+
+
+def update_expense(db: Session,expense_id: int, update_data: ExpenseUpdate): 
+    expense = db.query(Expense).filter(Expense.id == expense_id).first()
+
+    if not expense:
+        return None
+    
+    if update_data.title:
+        expense.title = update_data.title
+    if update_data.amount:
+        expense.amount = update_data.amount
+    if update_data.category:
+        expense.category = update_data.category
+    if update_data.date:
+        expense.date = update_data.date
+    if update_data.description:
+        expense.description = update_data.description
+    if update_data.receipt_url:
+        expense.receipt_url = update_data.receipt_url
+    
+    db.add(expense)
+    db.commit()
+    db.refresh(expense)
+    return expense
+
+
+def delete_expense(db: Session,expense_id: int):
+    expense = db.query(Expense).filter(Expense.id == expense_id).first()
+
+    if not expense:
+        return None
+    
+    db.delete(expense)
+    db.commit()
+    return expense
